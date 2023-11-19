5c9b52751c5dbc337381c7817f349ff539b9bfc6
Kevin Fuller <kgfuller@yahoo.com>
2023-11-18 21:56:28 -0500

    adding multiple players

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,8 +1,141 @@
 const socket = io();
 
 const connectButton = document.getElementById("connect");
-const username = document.getElementById("username")
-connectButton.addEventListener("click", ()=>{
-    if(!username.value) return;
-    socket.emit("setUsername", username.value)
-})
+const usernameInput = document.getElementById('username');
+let defaultRoom = "iceburg";
+
+connectButton.addEventListener("click", () => {
+    const username = usernameInput.value;
+    const room = defaultRoom;
+
+    if (username && room) {
+        socket.emit("setUsername", { username, room });
+    }
+});
+
+const canvas = document.getElementById('game');
+const context = canvas.getContext('2d');
+
+let currentRoom;
+let targetPositions = {};
+let lastTimestamp;
+
+function render() {
+    // Clear the canvas before drawing the updated positions
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw a square for each user based on their target position
+    for (const userId in currentRoom) {
+        const user = currentRoom[userId];
+        const targetPosition = targetPositions[userId] || { x: user.x, y: user.y }; // Use target position if available
+
+        context.fillStyle = 'blue';
+        context.fillRect(targetPosition.x, targetPosition.y, 15, 15);
+
+        // Draw the username below the square
+        context.fillStyle = 'black';
+        context.font = '10px Arial';
+        context.fillText(user.username, targetPosition.x, targetPosition.y + 20);
+    }
+}
+
+// Event: Handle mouse click on the canvas
+canvas.addEventListener('click', (event) => {
+    const rect = canvas.getBoundingClientRect();
+    const targetPosition = {
+        x: event.clientX - rect.left,
+        y: event.clientY - rect.top
+    };
+
+    // Send the target position to the server
+    socket.emit('move', targetPosition);
+});
+
+function animateMovement(userId, targetPosition, timestamp) {
+    // Initialize target position if not present
+    if (!targetPositions[userId]) {
+        targetPositions[userId] = { ...currentRoom[userId] };
+    }
+
+    // Set the target position for animation
+    targetPositions[userId].targetPosition = targetPosition;
+
+    // Start the animation
+    lastTimestamp = timestamp;
+    requestAnimationFrame(() => {
+        animateUser(userId);
+    });
+}
+
+function animateUser(userId) {
+    const user = targetPositions[userId];
+    const targetPosition = user.targetPosition;
+
+    // Calculate progress based on elapsed time
+    const elapsed = performance.now() - lastTimestamp;
+    const progress = Math.min(1, elapsed / 1000); // Limit progress to 1
+
+    // Interpolate position based on progress
+    user.x = user.x + (targetPosition.x - user.x) * progress;
+    user.y = user.y + (targetPosition.y - user.y) * progress;
+
+    // Render the updated positions
+    render();
+
+    // Continue the animation until reaching the target position
+    if (progress < 1) {
+        requestAnimationFrame(() => {
+            animateUser(userId);
+        });
+    }
+}
+
+// Event: Handle mouse click on the canvas
+canvas.addEventListener('click', (event) => {
+    const rect = canvas.getBoundingClientRect();
+    const targetPosition = {
+        x: event.clientX - rect.left,
+        y: event.clientY - rect.top
+    };
+
+    // Send the target position to the server
+    socket.emit('move', targetPosition);
+
+    // Animate the movement
+    animateMovement(socket.id, targetPosition, performance.now());
+});
+
+socket.on("roomUpdate", (roomData) => {
+    currentRoom = roomData;
+
+    // Update target positions
+    for (const userId in currentRoom) {
+        if (!targetPositions[userId]) {
+            targetPositions[userId] = { ...currentRoom[userId] };
+        } else {
+            targetPositions[userId].x = currentRoom[userId].x;
+            targetPositions[userId].y = currentRoom[userId].y;
+        }
+    }
+
+    // Render the updated positions
+    render();
+});
+
+// Event: Handle the addition of new users
+socket.on("userAdded", (userId, userData) => {
+    // Add the new user to the currentRoom object
+    currentRoom[userId] = userData;
+
+    // Render the updated positions
+    render();
+});
+
+// Event: Handle the removal of users
+socket.on("userRemoved", (userId) => {
+    // Remove the user from the currentRoom object
+    delete currentRoom[userId];
+
+    // Render the updated positions
+    render();
+});
--- src/index.js
+++ src/index.js
@@ -9,27 +9,87 @@ const io = new Server(httpServer, { /* options */ });
 
 app.use(express.static('public'));
 
-const users = {};
-
+// rooms will store a list of users and their positions.
+let rooms = {
+    iceburg: {},
+    plaza: {},
+    igloo: {},
+}
 
 io.on("connection", (socket) => {
-  console.log('a user connected')
+    console.log(`A new user connected: ${socket.id}`);
+    console.log("Current users:");
+    console.log(rooms);
 
-  socket.on("setUsername", (username)=>{
-    console.log(username)
+    socket.on("setUsername", ({ username, room }) => {
+        console.log(`A user set username: ${username} in room: ${room}`);
 
-    users[socket.id] = {
-        username,
-    }
-    console.log(users)
-  })
-  
-  socket.on("disconnect", function(){
-    console.log('user disconnected')
-    delete users[socket.id]
-  })
+        // Check if the requested room exists
+        if (!rooms[room]) {
+            console.log(`Invalid room: ${room}`);
+            return;
+        }
+
+        // Join the room before adding the user
+        socket.join(room);
+
+        // Add the user to the specified room
+        rooms[room][socket.id] = {
+            username,
+            x: 0,
+            y: 0,
+        };
+
+        // Emit the room information to the user
+        io.to(socket.id).emit("roomUpdate", rooms[room]);
+
+        // Broadcast the addition of a new user to others in the room
+        socket.to(room).emit("userAdded", socket.id, rooms[room][socket.id]);
+
+        console.log("Updated rooms:", rooms);
+    });
+
+    socket.on("move", (position) => {
+        // Update the user's position in the specified room
+        const roomsArray = Array.from(socket.rooms);
+        const room = roomsArray.length > 1 ? roomsArray[1] : undefined;
+
+        if (room && rooms[room] && rooms[room][socket.id]) {
+            rooms[room][socket.id].x = position.x;
+            rooms[room][socket.id].y = position.y;
+
+            // Emit the updated position only to users in the same room
+            io.to(room).emit("roomUpdate", rooms[room]);
+        }
+    });
+
+    // Event: Disconnect
+    socket.on("disconnect", function () {
+        console.log('User disconnected');
+
+        // Remove the user from the specified room
+        for (const room in rooms) {
+            if (rooms[room][socket.id]) {
+                // Broadcast the removal of a user to others in the room
+                socket.to(room).emit("userRemoved", socket.id);
+
+                delete rooms[room][socket.id];
+
+                // Emit the updated room information after a user disconnects
+                io.to(room).emit("roomUpdate", rooms[room]);
+            }
+        }
+    });
 });
 
-httpServer.listen(PORT, ()=>{
+function getTargetPositions(room) {
+    const targets = {};
+    for (const userId in rooms[room]) {
+        targets[userId] = rooms[room][userId].targetPosition || { x: rooms[room][userId].x, y: rooms[room][userId].y };
+    }
+    return targets;
+}
+
+httpServer.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 });
